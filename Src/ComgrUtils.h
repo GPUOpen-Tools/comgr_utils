5cdc417b52bdff1b7754c796e5ede5d318a10926
abenmos <Amit.Ben-Moshe@amd.com>
2025-03-07 15:01:58 -0500

    Update AMD copyrights.

--- Src/ComgrUtils.h
+++ Src/ComgrUtils.h
@@ -1,5 +1,5 @@
 //============================================================================================
-// Copyright (c) 2018 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
 /// \author AMD Developer Tools
 /// \file
 /// \brief  This is a high level C++ interface of comgr utility functionality for tools.
